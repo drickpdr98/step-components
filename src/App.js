28d4c77ee3d03ebada450735621dc3cfc57a94c4
drickpdr98 <dravinsookraj@gmail.com>
2023-06-26 20:37:58 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+  ];
+
+  const [step, setStep] = useState(1);
+
+  const nextMessage = function () {
+    if (step < 3) setStep((prev) => prev + 1);
+  };
+
+  const prevMessage = function () {
+    if (step > 1) setStep((prev) => prev - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="message">{messages[step - 1]}</p>
+      <div className="buttons">
+        <button
+          onClick={prevMessage}
+          style={{
+            backgroundColor: "#7950F2",
+            color: "fff",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Previous
+        </button>
+        <button
+          onClick={nextMessage}
+          style={{
+            backgroundColor: "#7950F2",
+            color: "fff",
+          }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
